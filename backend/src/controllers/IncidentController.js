7d1dc7b99537588ea7d7f84e0fa6e5d945a00d49
thallesdaniell <thallesdaniell@hotmail.com>
2020-03-26 12:33:38 -0300

    add table ongs join incidents

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -8,9 +8,16 @@ module.exports = {
         const [count] = await connection('incidents').count();
 
         const incidents = await connection('incidents')
+            .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
             .limit(5)
             .offset((page - 1) * 5)
-            .select('*');
+            .select(['incidents.*',
+                'ongs.name',
+                'ongs.email',
+                'ongs.whastapp',
+                'ongs.city',
+                'ongs.uf'
+            ]);
 
         response.header('X-Total-Count', count['count(*)']);
 
