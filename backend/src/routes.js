54d335ed98ec0117498253a2e59e8471ea18521a
thallesdaniell <thallesdaniell@hotmail.com>
2020-03-26 02:35:16 -0300

    routes create and list ongs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,30 @@
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('./databse/connection');
+
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.json({
-        evento: 'Semana OmniStak 11',
-        aluno: 'Thalles'
+routes.get('/ongs', async (request, response) => {
+    const ongs = await connection('ongs').select('*');
+    return response.json(ongs);
+});
+
+routes.post('/ongs', async (request, response) => {
+
+    const { name, email, whastapp, city, uf } = request.body;
+    const id = crypto.randomBytes(4).toString('HEX');
+    console.log(id);
+
+    await connection('ongs').insert({
+        id,
+        name,
+        email,
+        whastapp,
+        city,
+        uf
     });
+
+    return response.json({ id });
 });
 
 module.exports = routes;
