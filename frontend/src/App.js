4b676bbfc22766a0e263b9c39d55c140eae53c4a
thallesdaniell <thallesdaniell@hotmail.com>
2020-03-31 02:23:17 -0300

    adicionado conceito children

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,12 @@ import React from 'react';
 import Header from './Header';
 
 function App() {
+
+
   return (
-    <Header title="Semana OmniStack" />
+    <Header>
+      Semana OmniStack
+    </Header>
   );
 }
 
