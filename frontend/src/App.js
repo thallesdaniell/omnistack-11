22a9dcb7f2c9caa9ba728454884007a7c235161d
thallesdaniell <thallesdaniell@hotmail.com>
2020-03-31 02:30:30 -0300

    conceito estado e imutabilidade

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './Header';
 
 function App() {
+  let [counter, setCounter] = useState(0);
 
+
+  function increment() {
+    setCounter(counter + 1);
+  }
 
   return (
-    <Header>
-      Semana OmniStack
-    </Header>
+    <div>
+      <Header>Contador: {counter}</Header>
+      <button onClick={increment}>Incrementar</button>
+    </div>
   );
 }
 
